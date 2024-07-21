9b728695e8e984ca45177d7173af89dd5c0ed115
Amir Maghami <Maghami.A1987@gmail.com>
2024-07-21 14:09:13 +0330

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -83,43 +83,43 @@ export default defineNuxtConfig({
         },
       ],
       script: [
-        // {
-        //   src: "assets/js/vendor/jquery-3.6.0.min.js",
-        //   defer: true,
-        // },
-        // {
-        //   src: "assets/js/vendor/bootstrap.bundle.min.js",
-        //   defer: true,
-        // },
-        // {
-        //   src: "assets/js/vendor/jquery-migrate-3.3.2.min.js",
-        //   defer: true,
-        // },
+        {
+          src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.jsjquery-3.6.0.min.js",
+          defer: true,
+        },
+        {
+          src: "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.jsbootstrap.bundle.min.js",
+          defer: true,
+        },
+        {
+          src: "https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.3.2/jquery-migrate.min.js",
+          defer: true,
+        },
 
         // {
         //   src: "assets/js/vendor/modernizr-3.11.2.min.js",
         //   defer: true,
         // },
-        // {
-        //   src: "assets/js/plugins/jquery-ui.min.js",
-        //   defer: true,
-        // },
-        // {
-        //   src: "assets/js/plugins/swiper.min.js",
-        //   defer: true,
-        // },
-        // {
-        //   src: "assets/js/plugins/countdown.js",
-        //   defer: true,
-        // },
-        // {
-        //   src: "assets/js/plugins/scrollup.js",
-        //   defer: true,
-        // },
-        // {
-        //   src: "assets/js/plugins/elevateZoom.js",
-        //   defer: true,
-        // },
+        {
+          src: "https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.3/jquery-ui.min.js",
+          defer: true,
+        },
+        {
+          src: "https://cdnjs.cloudflare.com/ajax/libs/swipejs/2.3.1/swipe.min.js",
+          defer: true,
+        },
+        {
+          src: "https://cdnjs.cloudflare.com/ajax/libs/countdown/2.6.0/countdown.min.js",
+          defer: true,
+        },
+        {
+          src: "https://cdnjs.cloudflare.com/ajax/libs/scrollup/2.4.1/jquery.scrollUp.min.js",
+          defer: true,
+        },
+        {
+          src: "https://cdnjs.cloudflare.com/ajax/libs/elevatezoom/2.2.3/jquery.elevatezoom.min.js",
+          defer: true,
+        },
         {
           src: "assets/js/vendor/vendor.min.js",
           defer: true,
